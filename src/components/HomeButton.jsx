3139e6b6ecc14c5c51761905dfbc03f9fb53e19d
klara kindlund <>
2023-11-12 16:54:12 +0100

    added styling

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,11 +1,13 @@
-// Klara - styling and implementation for this component
+
 // Component with button that links back to home page
 import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import '../styles/HomeButton.css';
 
 export const HomeButton = ({props}) => {
     return(
-        <button>This is a button back to homepage!</button>
+        <Link to="/">Back</Link>
     )
 }
--- src/components/MovieThumbnail.jsx
+++ src/components/MovieThumbnail.jsx
@@ -1,21 +1,25 @@
-// Klara - styling for this component
 // Component for showing movie thumbnail (image, title, release date)
 import React from "react";
 import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import '../styles/MovieThumbnail.css';
 
 export const MovieThumbnail = ({ props }) => {
   return (
     <>
       {props.map((movieItem) => {
         return (
+          <div className="movie-thumbnail-container">
           <Link key={movieItem.id} to={`/movieDetails/${movieItem.id}`}>
-            <img
+            <img className="img"
               src={`http://image.tmdb.org/t/p/w300/${movieItem.poster_path}`}
               alt="movie poster"
             />
-            <p>{movieItem.title}</p>
+            <div className="movie-title">
+            <p className="movie-title-p">{movieItem.title}</p>
+            </div>
           </Link>
+          </div>
         );
       })}
     </>
